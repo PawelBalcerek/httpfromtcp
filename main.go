a45291c3d7bb3b4162bc2c7ec6eb186c2069fee8
pawelbalcerek <pawbal96@gmail.com>
2026-09-09 18:32:11 +0200

    read lines from messages.txt via channel

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		log.Panicf("%v", err)
+	}
+	defer file.Close()
+
+	for line := range getLinesChannel(file) {
+		fmt.Printf("read: %s\n", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+
+	go func() {
+		defer close(lines)
+
+		bytes := make([]byte, 8)
+		var currentLine strings.Builder
+		for {
+			n, err := f.Read(bytes)
+			if err != nil {
+				if currentLine.Len() != 0 {
+					lines <- currentLine.String()
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("an error has occurred: %s\n", err.Error())
+				return
+			}
+
+			str := string(bytes[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				lines <- fmt.Sprintf("%s%s", currentLine.String(), parts[i])
+				currentLine.Reset()
+			}
+			currentLine.WriteString(parts[len(parts)-1])
+		}
+	}()
+
+	return lines
+}
