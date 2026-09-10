55cb891b8f14443bb1df2f3b6d7dd72c8cd2abe2
pawelbalcerek <pawbal96@gmail.com>
2026-09-10 18:38:07 +0200

    parse chunked data

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,6 +1,7 @@
 package request
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"io"
@@ -10,6 +11,8 @@ import (
 
 type Request struct {
 	RequestLine RequestLine
+
+	requestState requestState
 }
 
 type RequestLine struct {
@@ -18,53 +21,112 @@ type RequestLine struct {
 	HttpVersion   string
 }
 
+type requestState int
+
+const (
+	requestStateInitialized requestState = iota
+	requestStateDone
+)
+
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	bytes, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read bytes: %w", err)
+	request := &Request{
+		requestState: requestStateInitialized,
 	}
-	requestLines := strings.Split(string(bytes), "\r\n")
-	if len(requestLines) < 1 {
-		return nil, errors.New("request is empty")
+	var toParse []byte
+	bytes := make([]byte, 8)
+	for request.requestState != requestStateDone {
+		readBytes, err := reader.Read(bytes)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				request.requestState = requestStateDone
+				break
+			}
+			return nil, fmt.Errorf("failed to read bytes: %w", err)
+		}
+
+		toParse = append(toParse, bytes[:readBytes]...)
+		parsedBytes, err := request.parse(toParse)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse bytes: %w", err)
+		}
+
+		if parsedBytes != 0 {
+			toParse = toParse[parsedBytes:]
+		}
 	}
-	startLine := requestLines[0]
-	requestLine, err := parseRequestLine(startLine)
-	if err != nil {
-		return nil, err
-	}
-	return &Request{
-		RequestLine: *requestLine,
-	}, nil
+	return request, nil
 }
 
-func parseRequestLine(startLine string) (*RequestLine, error) {
-	startLineParts := strings.Split(startLine, " ")
-	if len(startLineParts) != 3 {
-		return nil, errors.New("invalid request start line")
+func (r *Request) parse(data []byte) (int, error) {
+	switch r.requestState {
+	case requestStateInitialized:
+		parsedBytes, requestLine, err := requestLine(data)
+		if err != nil {
+			return 0, err
+		}
+
+		if requestLine != nil {
+			r.requestState = requestStateDone
+			r.RequestLine = *requestLine
+			return parsedBytes, nil
+		}
+
+		return parsedBytes, nil
+	case requestStateDone:
+		return 0, errors.New("request already in done state")
+	default:
+		return 0, errors.New("unknown request state")
 	}
-	method := startLineParts[0]
+}
+
+func requestLine(data []byte) (int, *RequestLine, error) {
+	idx := bytes.Index(data, []byte("\r\n"))
+	if idx == -1 {
+		return 0, nil, nil
+	}
+
+	requestLine, err := parseRequestLineText(string(data[:idx]))
+	if err != nil {
+		return 0, nil, err
+	}
+
+	return idx + 2, requestLine, nil
+}
+
+func parseRequestLineText(rlt string) (*RequestLine, error) {
+	requestLineParts := strings.Split(rlt, " ")
+	if len(requestLineParts) != 3 {
+		return nil, fmt.Errorf("invalid request line text: %s", rlt)
+	}
+
+	method := requestLineParts[0]
 	if len(method) == 0 {
 		return nil, errors.New("invalid method length")
 	}
+
 	for _, r := range method {
 		if !unicode.IsUpper(r) {
 			return nil, fmt.Errorf("unknown method character: %c", r)
 		}
 	}
-	httpVersionParts := strings.Split(startLineParts[2], "/")
+
+	httpVersionParts := strings.Split(requestLineParts[2], "/")
 	if len(httpVersionParts) != 2 {
 		return nil, errors.New("invalid http version literal")
 	}
+
 	if httpVersionParts[0] != "HTTP" {
 		return nil, errors.New("invalid http version")
 	}
+
 	httpVersion := httpVersionParts[1]
 	if httpVersion != "1.1" {
 		return nil, errors.New("invalid http version")
 	}
+
 	return &RequestLine{
 		Method:        method,
-		RequestTarget: startLineParts[1],
+		RequestTarget: requestLineParts[1],
 		HttpVersion:   httpVersion,
 	}, nil
 }
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -1,6 +1,7 @@
 package request_test
 
 import (
+	"io"
 	"strings"
 	"testing"
 
@@ -11,7 +12,10 @@ import (
 
 func TestRequestLineParse(t *testing.T) {
 	// Test: Good GET Request Line
-	r, err := request.RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	r, err := request.RequestFromReader(&chunkReader{
+		data:            "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 3,
+	})
 	require.NoError(t, err)
 	require.NotNil(t, r)
 	assert.Equal(t, "GET", r.RequestLine.Method)
@@ -19,7 +23,10 @@ func TestRequestLineParse(t *testing.T) {
 	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
 
 	// Test: Good GET Request Line with path
-	r, err = request.RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	r, err = request.RequestFromReader(&chunkReader{
+		data:            "GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 1,
+	})
 	require.NoError(t, err)
 	require.NotNil(t, r)
 	assert.Equal(t, "GET", r.RequestLine.Method)
@@ -43,6 +50,28 @@ func TestRequestLineParse(t *testing.T) {
 	require.Error(t, err)
 
 	// Test: Invalid http version
-	_, err = request.RequestFromReader(strings.NewReader("GET /coffee HTTP/2 \r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	_, err = request.RequestFromReader(strings.NewReader("GET /coffee HTTP/2\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.Error(t, err)
+}
+
+type chunkReader struct {
+	data            string
+	numBytesPerRead int
+	pos             int
+}
+
+// Read reads up to len(p) or numBytesPerRead bytes from the string per call
+// its useful for simulating reading a variable number of bytes per chunk from a network connection
+func (cr *chunkReader) Read(p []byte) (n int, err error) {
+	if cr.pos >= len(cr.data) {
+		return 0, io.EOF
+	}
+	endIndex := cr.pos + cr.numBytesPerRead
+	if endIndex > len(cr.data) {
+		endIndex = len(cr.data)
+	}
+	n = copy(p, cr.data[cr.pos:endIndex])
+	cr.pos += n
+
+	return n, nil
 }
